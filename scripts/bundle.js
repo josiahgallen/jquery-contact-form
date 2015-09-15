ad8d9c6d2bd1ad01189113ef39db554fd6e37da0
Josiah Allen <josiah_g_allen@yahoo.com>
2015-09-15 17:15:43 -0500

    normal mode complete

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -4,35 +4,52 @@
 $(document).ready(function () {
 
     var $form = $('form');
-    var $submit = $('button');
+    var $recieved = $('#recieved');
     var errorsArray = ['#error1', '#error2', '#error3', '#error4'];
     var infoArray = ['#name', '#email', '#website', 'textarea'];
+    var userData = [];
 
     $form.submit(function (e) {
         e.preventDefault();
         var pass = false;
-        if (!pass) {
-            for (var i = 0; i < infoArray.length; i++) {
-                if (!$(infoArray[i]).val()) {
-                    $(errorsArray[i]).show();
-                } else {
-                    $(errorsArray[i]).hide();
-                }
-            }
-            if ($(infoArray[1]).val().indexOf('@') === -1) {
-                $(errorsArray[1]).show();
+        for (var i = 0; i < infoArray.length; i++) {
+            if (!$(infoArray[i]).val()) {
+                $(errorsArray[i]).show();
+                $(infoArray[i]).css({ borderLeft: 'solid red .3em' });
+                pass = true;
             } else {
-                $(errorsArray[1]).hide();
+                $(errorsArray[i]).hide();
+                $(infoArray[i]).css({ borderLeft: 'solid black .3em' });
+            }
+        }
+        if ($(infoArray[1]).val().indexOf('@') === -1) {
+            $(errorsArray[1]).show();
+            $(infoArray[1]).css({ borderLeft: 'solid red .3em' });
+            pass = true;
+        } else {
+            $(errorsArray[1]).hide();
+            $(infoArray[1]).css({ borderLeft: 'solid black .3em' });
+        }
+
+        if ($(infoArray[2]).val().indexOf('http://') === -1) {
+            $(errorsArray[2]).show();
+            $(infoArray[2]).css({ borderLeft: 'solid red .3em' });
+            pass = true;
+        } else {
+            $(errorsArray[2]).hide();
+            $(infoArray[2]).css({ borderLeft: 'solid black .3em' });
+        }
+        if (!pass) {
+            console.log('pass');
+            for (var i = 0; i < infoArray.length; i++) {
+                userData.push($(infoArray[i]).val());
+                $form.hide();
+                $recieved.text('Thanks for contacting us ' + userData[0] + '. We recieved your message and will contact you shortly.');
+                $recieved.show();
             }
 
-            if ($(infoArray[2]).val().indexOf('http://') === -1) {
-                $(errorsArray[2]).show();
-            } else {
-                $(errorsArray[2]).hide();
-            }
-            pass = !pass;
+            console.log(userData);
         }
-        console.log('passed');
     });
 });
 
